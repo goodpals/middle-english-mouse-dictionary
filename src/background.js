bc7ebcf7dcb44efc666b39fdd14a48442142a920
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-10-15 15:06:29 +0100

    app soft-on/off  changed to contextMenu

--- src/background.js
+++ src/background.js
@@ -1,1 +1,44 @@
 /* the background/service worker is to handle events, manage data, and perform actions that don’t require direct user interaction. */
+/// console.logs don't seem to work here, but necessary to run these operations from the background task
+
+! async function setStateFirstTime(){  
+  await browser.storage.local.set({ state: 'on' });
+  // const result = await browser.storage.local.get('state');
+  // console.log("setup on tabload: extension is " + result.state);
+}();
+
+/// Make the right-click menu that has an option to enable/disable functionality. 
+/// This way extension is "enabled" in the extension manager, but our functionality "soft locked" via a rClick menu.
+browser.runtime.onInstalled.addListener(async () => {
+  const currentState = await browser.storage.local.get('state');
+  browser.contextMenus.create({
+    id: "rightClickOptionsMenu",
+    title: "Middle English: " + (currentState.state == 'on' ? 'turn off' : 'turn on'),
+    contexts: ["all"], /// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/menus/ContextType
+  });
+});
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "rightClickOptionsMenu") {
+
+    const currentState = await browser.storage.local.get('state');
+    const newState = currentState.state == 'on' ? 'off' : 'on';
+    await browser.storage.local.set({state: newState});
+    // const checkmark = newState == 'on' ? true : false;
+    browser.contextMenus.update("rightClickOptionsMenu", {
+      title: "Middle English: " + (newState == 'on' ? 'turn off' : 'turn on'),
+      // checked: !info.checked,
+    });
+  }
+});
+
+
+
+// const currentState = await browser.storage.local.get('state');
+//     console.log("current: " + currentState.state);
+
+//     const newState = currentState.state == 'on' ? 'off' : 'on';
+//     await browser.storage.local.set({state: newState});
+
+//     const checkStateChange = await browser.storage.local.get('state');
+//     console.log("turned: " + checkStateChange.state);
--- src/state.js
+++ src/state.js
@@ -1,21 +1,15 @@
-/// at least for now, turn on (enable extension functionality) when the webpage is first loaded
-! async function setStateFirstTime(){  
-  await browser.storage.local.set({ state: 'on' });
-  const result = await browser.storage.local.get('state');
-  console.log("setup on tabload: extension is " + result.state);
-}();
-
 /// Just as a test for now: enable or disable the extension functionality with the letter D 
-document.addEventListener('keydown', async function(event) {
-  if(event.key.toLowerCase() === "d") {
-    console.log("The letter D was pressed: disabling extension functionality");
-    const currentState = await browser.storage.local.get('state');
-    if (currentState.state == 'on') {
-        await browser.storage.local.set({state: 'off'});
-      } else {
-        await browser.storage.local.set({state: 'on'});
-      }
-    const newState = await browser.storage.local.get('state');
-    console.log("turned " + newState.state);
-  }
-});
+// document.addEventListener('keydown', async function(event) {
+//   if(event.key.toLowerCase() === "d") {
+//     console.log("The letter D was pressed: disabling extension functionality");
+
+//     const currentState = await browser.storage.local.get('state');
+//     console.log("current: " + currentState.state);
+
+//     const newState = currentState.state == 'on' ? 'off' : 'on';
+//     await browser.storage.local.set({state: newState});
+
+//     const checkStateChange = await browser.storage.local.get('state');
+//     console.log("turned: " + checkStateChange.state);
+//   }
+// });
