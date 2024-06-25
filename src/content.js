8d8fd7bfb5b6a06f32a47cb3e2c72c6260248868
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2024-06-25 15:11:32 +0100

    Extension rules suck (#19)

--- src/content.js
+++ src/content.js
@@ -36,7 +36,7 @@ Once HTML is injected into the browser window from a content domain script, it e
       hideModal();
     } else {
       let printouts = {};
-      Object.entries(activeWords).forEach(([key, word]) => printouts[key] = dictionaryEntriesToHTMLtext(word, "modal", null));
+      Object.entries(activeWords).forEach(([key, word]) => printouts[key] = new_dictionaryEntriesToHTMLtext(word, "modal", null));
 
       const range = selection.getRangeAt(0);
       const rect = range.getBoundingClientRect(); 
@@ -58,6 +58,9 @@ Once HTML is injected into the browser window from a content domain script, it e
  * @returns {bool} hasChanged
  */
 function processSelection(selection) {
+
+  // logError('', ''); // for debug -- testing logError
+
   /** @type {Set<string>} */
   const prev = new Set(Object.keys(activeWords));
   const sel = new Set(selection.split(" "));
@@ -105,77 +108,234 @@ function searchDictionary(selectedWord) {
 
 
 
-/**
- * This franken-function receives a list of userWordListEntry class objects and uses their index value to get info from the dictionary, and returns it as formatted text.
- * @param {Array.<MatchedWordEntry>} entries
- * @param {string} mode either "modal" or "sidebar"
- * @param {PageInfo} pageData to be used with "sidebar" `mode`. Get extra pre-stored info about the page user is presently on
- * @returns {string} HTML text ready to be passed into a modal/sidebar HTML element constructor, or `null` if there are no entries to parse.
- */
-function dictionaryEntriesToHTMLtext(entries, mode, pageData) {
+function new_dictionaryEntriesToHTMLtext(entries, mode, pageData) {
   if (entries == null || entries == undefined) return;
-  
-  let marginaliaShown = false;
-  let text = "";
-  
-  // header text for modal
-  if (entries.length > 1  &&  mode != "sidebar") text += `<p class="textHeader">` + plaintextToFraktur("Possible Matches")+"</p>"; 
+  if (['sidebar', 'modal'].includes(mode) == false) return;
 
-  if (pageData != null  &&  mode == "sidebar") { // header text for sidebar
-    text += `<button id="${SIDEBAR_CLOSE_BUTTON_ID}"></button><br>`;
-    text += `<p class="textHeader">` + plaintextToFraktur(pageData.pageName) + "</p><br>";
+  let marginaliaShown = false;
+  let textCont = document.createElement('div');
+
+  // set up header
+  const header = document.createElement('p');
+  header.className = 'textHeader';
+  
+  let headerText = document.createElement('p');
+
+  if (entries.length > 1  &&  mode != "sidebar") {
+    headerText.textContent = plaintextToFraktur("Possible Matches");
+  }
+  if (pageData != null && mode == "sidebar") {
+    const btn = document.createElement('button');
+    btn.className = 'wordListSidebarButton'; // you have to use a custom class because JS can smd
+    btn.id = SIDEBAR_CLOSE_BUTTON_ID;
+    textCont.appendChild(btn);
+    headerText.textContent = plaintextToFraktur(pageData.pageName);
   }
 
-  // build word info data for each entry
+  header.appendChild(headerText);
+  textCont.appendChild(header);
+
+  // build word info content for each word, as an HTML element
   for (const [index, entry] of entries.entries()) {
     const dictEntry = dictionary[entry.lookupIndex];
-    
-    // const url = "https://quod.lib.umich.edu/m/middle-english-dictionary/dictionary?utf8=✓&search_field=anywhere&q=" + entry.usersSelectedWord;
-    // text += '<p class="wordData";><a style="font-weight:bold !important; href=\"' + url + "\"target=\"_blank\" rel=\"noopener\">" + entry.usersSelectedWord + "</a>";
 
+    const wordHeaderElem = document.createElement('div'); // Use a <div> instead of <p> for flexible layout
+    wordHeaderElem.className = "wordData";
+    wordHeaderElem.style.whiteSpace = "nowrap"; // Ensure elements stay in a single line if they exceed width
+    wordHeaderElem.style.display = "flex"; // Use flexbox for layout
+    wordHeaderElem.style.alignItems = "center"; // Center align items vertically
 
-    const url = "https://quod.lib.umich.edu/m/middle-english-dictionary/dictionary?utf8=✓&search_field=anywhere&q=" + entry.usersSelectedWord;
-  text += '<p class="wordData"><a style="font-weight:bold !important;" href="' + url + '" target="_blank" rel="noopener">' + entry.usersSelectedWord + '</a>';
+    const urlElem = document.createElement('a'); // MAKE ENTRY WORD CLICKABLE URL 
+    urlElem.style.fontWeight = "bold";
+    urlElem.textContent = entry.usersSelectedWord;
+    urlElem.style.marginRight = "5px";
+    urlElem.href = `https://quod.lib.umich.edu/m/middle-english-dictionary/dictionary?utf8=✓&search_field=anywhere&q=${entry.usersSelectedWord}`;
+    wordHeaderElem.appendChild(urlElem);
 
-
-    if (dictEntry.partOfSpeech != null) text += ": " + dictEntry.partOfSpeech;
-
-    if (mode != "sidebar") {
-      const id = entry.lookupIndex; // must assign this to a const var first
-      text += ` <button id="${MODAL_ADDWORD_BUTTON_ID_PREFIX}${id}" class="modalButton"></button> `;
+    if (dictEntry.partOfSpeech != null) {
+      const speechPartElem = document.createElement('span'); // <span> instead of <p> for inline elements
+      speechPartElem.textContent = `${dictEntry.partOfSpeech}`;
+      speechPartElem.style.marginRight = "5px";
+      speechPartElem.style.display = "inline-block"; // inline-block allows width and height settings
+      wordHeaderElem.appendChild(speechPartElem);
     }
-    text += "</p>";
 
-    if (dictEntry.variants != null) text += "<p class='wordData'>Variants: " + dictEntry.variants.join(", ") + "</p>";
-    if (dictEntry.entry != null) text += "<p class='wordData'>" + htmlize(dictEntry.entry) + "</p>";
+    const addWordBtn = document.createElement('button'); // ADD WORDLIST ADDER BUTTON TO HEADER
+    const lookupID = entry.lookupIndex;
+    addWordBtn.className = 'modalButton';
+    addWordBtn.id = `${MODAL_ADDWORD_BUTTON_ID_PREFIX}${lookupID}`;
+    addWordBtn.style.display = "inline-block"; // Display as inline-block to allow width and height settings
+    wordHeaderElem.appendChild(addWordBtn);
 
-    // This will display a single, randomised marginalia in any modal with >3 entries, in the middle of the entries.
-    // Around 20% of lookup words have > 1 indexes; ~5% have > 2. 
-    if ((mode == "modal")
+    // New <p> -- add variant spellings
+    const variantsElem = document.createElement('p');
+    variantsElem.className = 'wordData';
+    let variantsText = "";
+    if (dictEntry.variants != null) {
+      variantsText = `Variants: ${dictEntry.variants.join(", ")}`;
+    }
+    variantsElem.textContent = variantsText;
+
+    const entryElem = document.createElement('p');
+    entryElem.className = 'wordData';
+    if (dictEntry.entry != null) appendHtmlizedText(entryElem, dictEntry.entry);
+
+    const marginaliaElem = document.createElement('img');
+    marginaliaElem.className = "marginalia";
+    let src = "";
+    if ((mode == "modal") 
     &&  (marginaliaShown == false)
     &&  (entries.length > 2) 
     &&  (index+1 == Math.round(entries.length / 2))) {
-      let fullURL = browser.runtime.getURL(getRandomImagePath());
+      const fullURL = browser.runtime.getURL(getRandomImagePath());
       if (fullURL) {
-        text += `<img src="${fullURL}" style="width:80%;display:block; margin: 0 auto;">`;
+        src = fullURL;
         marginaliaShown = true;
       }
-    }
-
-    // Same as for modal, but then that marginalia will be persistent for that page's sidebar for that session.
+    } 
     if ((mode == "sidebar") 
     &&  (entries.length > 2) 
     &&  (index+1 == Math.round(entries.length / 2)  )) {
-      if (persistentSideBarMarginaliaURL == null) persistentSideBarMarginaliaURL = browser.runtime.getURL(getRandomImagePath());
-      text += `<img src="${persistentSideBarMarginaliaURL}" style="width:95%;display:block; margin: 0 auto;">`;
+      if (persistentSideBarMarginaliaURL == null) {
+        persistentSideBarMarginaliaURL = browser.runtime.getURL(getRandomImagePath());
+      }
+      src = persistentSideBarMarginaliaURL;
     }
-    
-    text += "<p class='wordData' style='padding-bottom:15px !important'>_____</p>";
+    marginaliaElem.src = src;
+
+    const dividerElem = document.createElement('p');
+    dividerElem.className = "wordData";
+    dividerElem.textContent = "_____";
+    dividerElem.style.paddingBottom = "10px";
+
+    // combine it all
+    textCont.appendChild(wordHeaderElem);
+    textCont.appendChild(variantsElem);
+    textCont.appendChild(entryElem);
+    textCont.appendChild(marginaliaElem);
+    textCont.appendChild(dividerElem);
   }
 
-  return text;
+  return textCont;
 }
 
 
 
 
+/**
+ * Parses a string and appends the appropriate text and HTML elements to the parent element.
+ * @param {Element} parentElement - The parent element to append to.
+ * @param {string} entry - The entry string to parse and append.
+ */
+function appendHtmlizedText(parentElement, entry) {
+  const boldRegex = /#([^#]+)#/g;
+  const italicRegex = /_([^_]+)_/g;
+
+  // A DIGITAL SCAN OF THE SOURCE BOOK, HOSTED ON ARCHIVE.ORG
+  const archiveLink_root = 'https://archive.org/details/sisamfourteenthcenturyverse/page/n';
+  const archiveLink_mode = '/mode/1up';
+  const pageAlignNum = 51; // the offset number of pages for each chapter number
+  const numeralCodes = new Map([
+    ['I', 1], // page numbers
+    ['II', 13],
+    ['III', 32],
+    ['IV', 36],
+    ['V', 44],
+    ['VI', 57],
+    ['VII', 68],
+    ['VIII', 76],
+    ['IX', 94],
+    ['X', 107],
+    ['XI', 115],
+    ['XII', 129],
+    ['XIII', 145],
+    ['XIV', 151],
+    ['XV', 162],
+    ['XVI', 171],
+    ['XVII', 185],
+  ]);
+
+
+  const fragments = [];
+
+  // First, replace bold text
+  let lastIndex = 0;
+  entry.replace(boldRegex, (match, p1, offset) => {
+    if (offset > lastIndex) {
+      fragments.push({ type: 'text', content: entry.slice(lastIndex, offset) });
+    }
+    fragments.push({ type: 'bold', content: p1 });
+    lastIndex = offset + match.length;
+  });
+
+  if (lastIndex < entry.length) {
+    fragments.push({ type: 'text', content: entry.slice(lastIndex) });
+  }
+
+  // now parse italics out
+  const processedFragments = [];
+  fragments.forEach(fragment => {
+    if (fragment.type === 'text') {
+      let text = fragment.content;
+      let lastItalicIndex = 0;
+      text.replace(italicRegex, (match, p1, offset) => {
+        if (offset > lastItalicIndex) {
+          processedFragments.push({ type: 'text', content: text.slice(lastItalicIndex, offset) });
+        }
+        processedFragments.push({ type: 'italic', content: p1 });
+        lastItalicIndex = offset + match.length;
+      });
+      if (lastItalicIndex < text.length) {
+        processedFragments.push({ type: 'text', content: text.slice(lastItalicIndex) });
+      }
+    } else {
+      processedFragments.push(fragment);
+    }
+  });
+
+  // now build in links into roman numerals
+  processedFragments.forEach(fragment => {
+    if (fragment.type === 'text') {
+      let text = fragment.content;
+      const romanNumeralKeys = Array.from(numeralCodes.keys());
+
+      // Process each word in the text
+      text.split(/(\s+)/).forEach(word => {
+        const cleanWord = word.replace(/^\(|\)$/, ''); // Remove parentheses from the left and right if present
+        if (romanNumeralKeys.includes(cleanWord)) {
+          const pageNumber = pageAlignNum + numeralCodes.get(cleanWord);
+          const anchorElem = document.createElement('a');
+          anchorElem.href = `${archiveLink_root}${pageNumber}${archiveLink_mode}`;
+          anchorElem.textContent = word; // Retain the original word with parentheses
+          parentElement.appendChild(anchorElem);
+        } else {
+          parentElement.appendChild(document.createTextNode(word));
+        }
+      });
+    } else if (fragment.type === 'bold') {
+      const boldElem = document.createElement('b');
+      boldElem.textContent = fragment.content;
+      parentElement.appendChild(boldElem);
+    } else if (fragment.type === 'italic') {
+      const italicElem = document.createElement('i');
+      italicElem.textContent = fragment.content;
+      parentElement.appendChild(italicElem);
+    }
+  });
+}
+
+
+
+function findAndReplaceNumerals(str) {
+  let result = str;
+  numeralCodes.forEach((value, key) => {
+      const findRx = new RegExp(`(?:, |> )${key}\\b`, 'g'); // looks for numerals preceded by the edge of a tag, or a comma
+      const replaceRx = new RegExp(`\\b${key}\\b`, 'g'); // just parses the roman numeral pattern
+      const matches = result.match(findRx);
+      if (!matches) return; // effectively a `continue;`
+      const linkifiedNumerals = buildLink(key, value);
+      const replacedStr = result.replace(replaceRx, linkifiedNumerals);
+      if (replacedStr !== result) result = replacedStr;
+  });
+  return result;
+}
--- src/sidebar.js
+++ src/sidebar.js
@@ -89,7 +89,8 @@ async function createSidebar() {
   sidebar.id = SIDEBAR_ID;
 
   const pageData = currentPagesList[url];
-  sidebar.innerHTML = dictionaryEntriesToHTMLtext(wordsToShow, "sidebar", pageData);
+  const htmlToPass =  new_dictionaryEntriesToHTMLtext(wordsToShow, "sidebar", pageData);
+  sidebar.appendChild(htmlToPass);
   
   document.body.appendChild(sidebar);
   
